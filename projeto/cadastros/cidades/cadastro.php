31ec7baa607015c4d84a87640ce5aaa4f89e8050
Alexsander Vermoehlen <alexsander.vermoehlen@unidavi.edu.br>
2021-11-17 20:16:07 -0300

    select no cadastro de cidade

--- projeto/cadastros/cidades/cadastro.php
+++ projeto/cadastros/cidades/cadastro.php
@@ -18,8 +18,27 @@
         <input type="text" class="form-control" name="codigo" id="codigo" placeholder="Codigo">
         <label for="nome">Nome</label>
         <input type="text" class="form-control" name="nome" id="nome" placeholder="Nome">
-        <label for="nome">Estado</label>
-        <input type="text" class="form-control" name="estado" id="estado" placeholder="Estado">
+
+        <?php
+        $stmt = $conn->prepare('SELECT * FROM estados');
+        $stmt->execute();
+        $result = $stmt->fetchAll();
+        ?>
+        <div class="form-group">
+            <label for="estado">Estado</label>
+            <select class="form-control" name="estado" id="estado">
+                <option value="">** Selecione **</option>
+                <?php
+                    foreach ($result as $l) {
+                        ?>
+                            <option selected value="<?=$l['id']?>"><?=$l['sigla']?> - <?=$l['nome']?></option>
+                        <?php
+                    }
+                ?>
+            </select>
+        </div>
+        <!--<label for="nome">Estado</label>
+        <input type="text" class="form-control" name="estado" id="estado" placeholder="Estado"> -->
     </div>
     <input type="submit" name="gravar" value="Gravar">
 </form>
